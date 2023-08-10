21301648b2c3858a9000f4158db78a25ea3b9ac6
Bruno Filho <oliveira.brunofilho@gmail.com>
2023-08-10 14:12:58 -0300

    beta zoom functions removed

--- src/components/ImageDialog/index.tsx
+++ src/components/ImageDialog/index.tsx
@@ -1,5 +1,5 @@
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
 
 interface ImageDialogProps {
   children: ReactNode
@@ -7,18 +7,6 @@ interface ImageDialogProps {
 }
 
 export function ImageDialog({ children, src }: ImageDialogProps) {
-  const [zoomPosition, setZoomPosition] = useState({ x: 0, y: 0 })
-  const [showZoom, setShowZoom] = useState(false)
-  const zoomFactor = 10
-
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    const { left, top, width, height } =
-      event.currentTarget.getBoundingClientRect()
-    const x = (event.clientX - left) / width
-    const y = (event.clientY - top) / height
-    setZoomPosition({ x, y })
-  }
-
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
